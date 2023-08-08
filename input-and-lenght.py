e9e4fd5d8de6609065b64a971d2d2e7c3b2d5677
sanjay kumar <141133625+sanjaykumar1409@users.noreply.github.com>
2023-08-08 13:45:59 +0530

    Create input-and-lenght.py

---
+++ input-and-lenght.py
@@ -0,0 +1,7 @@
+#input function
+name= input("what is your name?\n")#takes the input from the user
+
+print(name)#prints the name given by the user
+
+#lenght function
+print(len("among us"))#prints the no.of characters in the word including spaces
